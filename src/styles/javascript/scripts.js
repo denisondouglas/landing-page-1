2ff7831c2ae5e103946560539ed577609d6c909b
denisonsouglas <deouglas22@gmail.com>
2024-10-04 11:18:59 -0300

    Projeto landing page finalizado.

--- src/styles/javascript/scripts.js
+++ src/styles/javascript/scripts.js
@@ -2,4 +2,52 @@ $(document).ready(function () {
   $("#mobile-btn").on("click", function () {
     $("#mobile-menu").toggleClass("active");
   });
+
+  const sections = $("section");
+  const navItens = $(".nav-item active");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+
+    let activeSectionIndex = 0;
+
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 1px 5px rgba(0, 0, 0, 0.1");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 96;
+      const sectionBottom = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+        activeSectionIndex = i;
+        return false;
+      }
+    });
+
+    $(navItens[activeSectionIndex]).addClass("active");
+  });
+
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    duration: 2000,
+    distance: "20%",
+  });
+
+  ScrollReveal().reveal("#testimonials", {
+    origin: "top",
+    duration: 2000,
+    distance: "20%",
+  });
+
+  ScrollReveal().reveal("#feedback", {
+    origin: "top",
+    duration: 2000,
+    distance: "20%",
+  });
 });
